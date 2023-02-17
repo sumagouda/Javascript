4298e7148d3a85da6b169d09610649185275d8ab
sumagouda <sumagouda532@gmail.com>
2023-02-17 15:45:49 +0530

    js

---
+++ arithmatic expressions.js
@@ -0,0 +1,29 @@
+let students = 24;
+
+// students = students + 2;
+// console.log(students);
+// students = students - 3;
+// console.log(students);
+// students = students * 2;
+// console.log(students);
+// students = students / 5;
+// console.log(students);
+// students = students >30;
+// console.log(students);
+
+
+// let extraStudents = students % 3;
+// console.log(extraStudents);
+
+
+// students +=2;
+// console.log(students);
+// students -=3;
+// console.log(students);
+// students *=2;
+// console.log(students);
+// students /=5;
+// console.log(students);
+
+//  extraStudents %= 3;
+// console.log(extraStudents);
---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log("I like pizza");
+// console.log("It's really good!");
+
+// window.alert("I REALLY LOVE PIZZA!");
+
+// let firstName="Suma";
+// let age=23;
+// console.log(firstName);
+// console.log(age);
+
+
+let firstName="Suma";
+let age=21;
+let student = true;
+
+console.log("Hello",firstName);
+console.log("you are",age,"years old");
+console.log("Enarolled:", student);
+
+document.getElementById("p1").innerHTML = "Hello" +firstName;
+document.getElementById("p2").innerHTML = "You are" +age +"years old";
+document.getElementById("p3").innerHTML = "Enrolled: " +student;
